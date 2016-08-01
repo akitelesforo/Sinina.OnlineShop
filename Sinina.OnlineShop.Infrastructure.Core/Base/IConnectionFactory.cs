f51612d2728c99dfbf68b2d410d5acf9bb230c9a
Akito Telesforo <akitelesforo@yahoo.com>
2016-08-01 16:47:10 +0800

    Added Base Class

---
+++ Sinina.OnlineShop.Infrastructure.Core/Base/IConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinina.OnlineShop.Infrastructure.Core.Base
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection Create();
+    }
+}
---
+++ Sinina.OnlineShop.Infrastructure.Core/Base/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Sinina.OnlineShop.Infrastructure.Core.Base
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void SaveChanges();
+    }
+}
